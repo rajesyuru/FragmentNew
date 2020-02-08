02b0c871930c8ddd7c8b56b9f84cf0ecb1463e73
sherl <sherlockeria@gmail.com>
2020-02-08 12:59:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new FragmentActivity()).commit();
+
+        Button button = findViewById(R.id.buttonThere);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
